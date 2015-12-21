b6cdb516511bc34d31b4c80571d02e06f2c45d4c
siddontang <siddontang@gmail.com>
2015-12-21 08:19:46 +0800

    use jepsen_dind instead

--- run_dind.sh
+++ run_dind.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --privileged -t -i -v $(pwd):/jepsen_tidb --name jepsen siddontang/jepsen_tidb
+docker run --privileged -t -i -v $(pwd):/jepsen_tidb --name jepsen_dind siddontang/jepsen_dind
